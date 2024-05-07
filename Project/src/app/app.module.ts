152db888336f367a27014b6eed0b3e03ba90b584
fatima <fatima@nahaus.de>
2024-05-07 20:07:30 +0300

    inject firestore

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {getApp, initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence(),    AngularFireStorageModule,
+    provideStorage(() => getStorage()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
