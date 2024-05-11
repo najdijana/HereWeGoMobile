c430bc1b6aef561845cb95ee91dbf6bab8b5b1c4
fatima <fatima@nahaus.de>
2024-05-11 18:08:19 +0300

    loader page

---
+++ Project/src/app/modules/auth/loader/loader.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.page.html',
+  styleUrls: ['./loader.page.scss'],
+})
+export class LoaderPage implements OnInit {
+
+  constructor(private fireauth: AngularFireAuth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.fireauth.authState.subscribe(user => {
+      if (user) {
+        this.router.navigate(['folder/Inbox']); // Adjust as necessary for default folder
+      } else {
+        this.router.navigate(['sign-in']);
+      }
+    });
+  }
+
+}
