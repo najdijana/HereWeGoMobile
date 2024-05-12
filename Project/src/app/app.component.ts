4e7a811a35d44324e3e345a0dff59fa0eea49362
fatima <fatima@nahaus.de>
2024-05-12 17:41:30 +0300

    update app

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'All Destinations', url: '/topdestinations-leb', icon: 'mail' },
+    { title: 'Destinations', url: '/top-destinations', icon: 'paper-plane' },
+    { title: 'Packages', url: '/packages', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(public auth:AuthService) {}
+  ngOnInit(): void {
+   
+  }
 }
