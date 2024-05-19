12bf504f67cb64310293ea291f7ed4e9d6af729d
Jana Najdi <najdijana432@gmail.com>
2024-05-19 17:03:20 +0300

    chatbot

--- Project/src/app/folder/folder.module.ts
+++ Project/src/app/folder/folder.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -20,6 +20,7 @@ import { ComponentsModule } from '../modules/admin/chats/components.module';
     //ChatbotComponent,
     ComponentsModule
   ],
-  declarations: [FolderPage, ServicesComponent, ChatbotComponent]
+  declarations: [FolderPage, ServicesComponent, ChatbotComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FolderPageModule {}
