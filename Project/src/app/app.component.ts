c2ec2560259f09caf3ec79a1abf65422ee42ecc4
fatima <fatima@nahaus.de>
2024-05-20 21:27:10 +0300

    app

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -7,16 +7,19 @@ import { AuthService } from './shared/services/auth.service';
 })
 export class AppComponent implements OnInit {
   public appPages = [
+    { title: 'home', url: '/loader', icon: 'trash' },
     { title: 'All Destinations', url: '/topdestinations-leb', icon: 'mail' },
     { title: 'Destinations', url: '/top-destinations', icon: 'paper-plane' },
     { title: 'Packages', url: '/packages', icon: 'heart' },
     { title: 'favorites', url: '/favorites', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'bookings', url: '/bookings', icon: 'warning' },
+    { title: 'user Profile', url: '/user-profile/:id', icon: 'warning' },
+
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor(public auth:AuthService) {}
   ngOnInit(): void {
-   
+      // this.auth.logout()
+
   }
 }
