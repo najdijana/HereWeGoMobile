f36c57518bff5852e892b722a0b4bd461842e209
fatima <fatima@nahaus.de>
2024-05-24 20:27:24 +0300

    interface

--- Project/src/app/shared/models/review.interface.ts
+++ Project/src/app/shared/models/review.interface.ts
@@ -1,11 +1,12 @@
-import { Plan } from "./packages.interface";
 import { User } from "./user.interface";
 
 export interface Review{
     id?:string;
     rate?:number;
     review?:string;
-    plan?:Plan[];
+    description?:string[];
     user?:User;
-    startDate:Date;
+    ratedAt?:Date;
 }
+
+export type Reviews = Review[];
