9e3459f95a1a46bc3b2add5f932714731351faf6
fatima <fatima@nahaus.de>
2024-05-25 22:57:33 +0300

    fav

--- Project/src/app/modules/admin/favorites/favorites.page.ts
+++ Project/src/app/modules/admin/favorites/favorites.page.ts
@@ -7,6 +7,9 @@ import { ActivitiesService } from '../activities/services/activities.services';
 import { RestaurantsService } from '../restaurants/restaurants.service';
 import { TransportationService } from '../transportations/transportation.service';
 import { AccommodationService } from '../accomodations/accomodations.service';
+import { User } from 'src/app/shared/models/user.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-favorites',
@@ -18,25 +21,27 @@ export class FavoritesPage implements OnInit,OnDestroy {
   searchQuery$: BehaviorSubject<string> = new BehaviorSubject('');
   filteredFavorites$: Observable<any[]>;
   private subscriptions: Subscription = new Subscription();
+  user: User;
 
 
   constructor(private firestore:AngularFirestore,
+              private userService:UserService,
+              private authServie:AuthService,
               private topDestinationService:TopDestinationService,
               private activitiesService:ActivitiesService,
               private restaurantsService:RestaurantsService,
               private transportationService:TransportationService,
               private accomodationService:AccommodationService) { }
 
-
-           
-
   ngOnInit() {
+    this.user=this.authServie.authUser;
+    console.log("user",this.user);
     this.favorites$ = this.getAllFavorites();
     this.filteredFavorites$ = combineLatest([this.favorites$, this.searchQuery$]).pipe(
       map(([favorites, query]) => {
         return favorites.filter(item => 
-          item.Name.toLowerCase().includes(query) ||
-          item.City.toLowerCase().includes(query)
+          item?.Name?.toLowerCase().includes(query) ||
+          item?.City?.toLowerCase().includes(query)
         );
       })
     ); }
@@ -46,7 +51,7 @@ export class FavoritesPage implements OnInit,OnDestroy {
   }
 
   private fetchFavorites(collection: string): Observable<any[]> {
-    return this.firestore.collection(collection, ref => ref.where('isFavorite', '==', true))
+    return this.firestore.collection(`users/${this.user.uid}/${collection}`)
       .snapshotChanges()
       .pipe(
         map(actions => actions.map(a => {
@@ -61,7 +66,6 @@ export class FavoritesPage implements OnInit,OnDestroy {
     const query = event.target.value.toLowerCase();
     this.searchQuery$.next(query);
   }
-  
 
   // Fetch all favorites across collections
   getAllFavorites(): Observable<any[]> {
@@ -74,31 +78,27 @@ export class FavoritesPage implements OnInit,OnDestroy {
   }
 
   toggleFavorite(item: any) {
-
     console.log("Selected item:", item);
-
+    
     switch (item.type) {
-      case 'Accomodation':
-        this.accomodationService.doc(item.id).update({ isFavorite: false });
-          break;
+      case 'Accommodation':
+        this.userService.collection().doc(this.user?.uid).collection('Accommodation').doc(item.id).delete();
+        break;
       case 'Activities':
-        this.activitiesService.doc(item.id).update({ isFavorite: false });
-          break;
+        this.userService.collection().doc(this.user?.uid).collection('Activities').doc(item.id).delete();
+        break;
       case 'Restaurants':
-        this.restaurantsService.doc(item.id).update({ isFavorite: false });
-          break;
+        this.userService.collection().doc(this.user?.uid).collection('Restaurants').doc(item.id).delete();
+        break;
       case 'Transportation':
-        this.transportationService.doc(item.id).update({ isFavorite: false });
-          break;
+        this.userService.collection().doc(this.user?.uid).collection('Transportation').doc(item.id).delete();
+        break;
       case 'TopDestinations':
-        this.topDestinationService.doc(item.id).update({ isFavorite: false });
+        this.userService.collection().doc(this.user?.uid).collection('TopDestinations').doc(item.id).delete();
         break;
       default:
-          break;
-  }
-    
-
-    
+        break;
+    }
   }
 
   handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
