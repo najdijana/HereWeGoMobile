d6a090a1587934d013712dc5313bea1fdf72f539
fatima <fatima@nahaus.de>
2024-05-25 22:57:24 +0300

    fav

--- Project/src/app/modules/admin/favorites/favorites-routing.module.ts
+++ Project/src/app/modules/admin/favorites/favorites-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FavoritesPage } from './favorites.page';
+import { UserDataResolver } from 'src/app/app.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: FavoritesPage
+    component: FavoritesPage,
+    resolve:{
+      user:UserDataResolver
+    }
   }
 ];
 
